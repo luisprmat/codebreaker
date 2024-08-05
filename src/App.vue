2492f0b19bedf0f3af9e72b01e2762fadc391fb6
Luis Parrado <luisprmat@gmail.com>
2024-08-04 22:26:13 -0500

    Create my first Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+import CreateCode from './components/CreateCode.vue'
+</script>
+
 <template>
   <h1 class="text-xl text-red-500">Hello world!</h1>
+
+  <CreateCode />
 </template>
---
+++ src/components/CreateCode.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+  if (window.confirm('¿Está seguro de hacer eso?')) {
+    count.value++
+  }
+}
+</script>
+
+<template>
+  <div>
+    {{ count }}
+
+    <p>
+      <button @click="increment">Aumentar</button>
+    </p>
+  </div>
+</template>
